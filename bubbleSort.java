3e0244e152b035024d6722f4f8911f93fc0c4314
Ashutosh-Bhardwaj-26 <68807593+Ashutosh-Bhardwaj-26@users.noreply.github.com>
2022-03-23 15:49:46 +0530

    LAB-4
    Bubble sort implementation.

---
+++ bubbleSort.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class bubbleSort {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+
+        ArrayList<Integer> bs = new ArrayList<>();
+        int n = sc.nextInt();
+
+        for(int i =0;i<n;i++){
+            bs.add(sc.nextInt());
+        }
+
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (bs.get(j) > bs.get(j+1))
+                {
+                    // swap arr[j+1] and arr[j]
+                    int temp = bs.get(j);
+                    bs.set(j,bs.get(j+1));
+                    bs.set(j+1,temp);
+                }
+        sc.close();
+
+        System.out.println(bs);
+
+    }
+}   
